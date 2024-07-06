53c67aea180d36496a356a9b23487542829f2c17
abbassimedayoub <abbassimedayoub@gamil.com>
2024-07-06 21:25:27 +0200

    setting file of my project django

--- Santech_dajngo/Santech/settings.py
+++ Santech_dajngo/Santech/settings.py
@@ -18,7 +18,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '3zue&@nv@q_*70)!#ntcfxn8k(k8xa#o-q+jzk$l&+xj%)(51r'
